605a2d29f629bdad8f20015bc3308f995e7ec0aa
Daniel Kraus <krada@gmx.net>
2012-07-16 00:15:01 +0200

    Mail alias

--- configure-server.sh
+++ configure-server.sh
@@ -17,7 +17,7 @@ yesno() {
 	local choice="yn"
 	[[ $(echo $3 | grep -i y) ]] && local choice="Yn"
 	[[ $(echo $3 | grep -i n) ]] && local choice="yN"
-	echo -n $1" [$choice] "
+	echo -n $bold$1$normal" [$choice] "
 	local answer=x
 	until [[ -z "$answer" || $(echo $answer | grep -i [yn]) ]]; do
 		read -s -n 1 answer
@@ -25,8 +25,15 @@ yesno() {
 	eval $2="$answer"
 	[ -z $answer ] && eval $2="$3"
 	echo $answer
-	[ $(echo $3 | grep -i $answer) ] && return 1
+	[[ $(echo $answer | grep -i $3) ]] && return 1
+	return 0
 }
+
+
+
+# ########################################################################
+# Find out about the current environment
+# ########################################################################
 
 echo $bold"This will configure the Ubuntu server."$normal
 
@@ -60,13 +67,18 @@ else # not running in a Virtual Box
 	if [ -z "$SSH_CLIENT" ]; then
 		echo $bold"You appear to be on a remote desktop computer."$normal
 		yesno "Copy the script to the server and log into the SSH?" answer y
-		if [ $? ]; then
+		if [[ $? ]]; then
 			read -p "Please enter user name on server: " -e -i $remote_user remote_user
 			read -p "Please enter server name: " -e -i $server server
 			echo $bold"Copying this script to the remote user's home directory..."$normal
 			scp $0 $remote_user@$server:.
-			echo $bold"Logging into server using SSH..."$normal
-			ssh $remote_user@$server
+			if [[ $? ]]; then
+				echo $bold"Logging into server using SSH..."$normal
+				ssh $remote_user@$server
+			else
+				echo "Failed to copy the file. Please check that the server is running "
+				echo "and the credentials are correct."
+			fi
 			exit
 		fi
 	else
@@ -76,14 +88,17 @@ fi
 
 
 # #####################################################################
-# Now let's configure the server
+# Now let's configure the server. 
+# Everything below this comment should only be executed on a running
+# server. (The above parts of the script should make sure this is the
+# case.)
 # #####################################################################
 
 # Prevent Grub from waiting indefinitely for user input on a headless server.
 
 if [ $(grep "set timeout=-1" /etc/grub.d/00_header) ]; then
 	yesno "Patch Grub to not wait for user input when booting the system?" answer y
-	if [ $? ]; then
+	if (( $? )); then
 		echo $bold"Patching Grub..."$normal
 		patch /etc/grub.d/00_header <<EOF
 --- 00_header	2012-04-17 20:20:48.000000000 +0200
@@ -102,6 +117,48 @@ EOF
 	fi
 else
 	echo "Grub is already patched."
+fi
+
+
+# Restrict sudo usage to the current user
+
+if [[ ! $(sudo grep "^$(whoami)" /etc/sudoers) ]]; then
+	yesno "Make $(whoami) the only sudoer?" answer y
+	if (( $? )); then
+		echo $bold"Patching /etc/sudoers"$normal
+		# To be on the safe side, we patch a copy of /etc/sudoers and only
+		# make the system use it if it passes the visudo test.
+		sudo sed 's/^\(%admin\|root\|%sudo\)/#&/'  /etc/sudoers > configure-sudoers.tmp
+		echo 'daniel	ALL=(ALL:ALL) ALL ' | sudo tee -a configure-sudoers.tmp > /dev/null
+
+		# Visudo returns 0 if everything is correct; 1 if errors are found
+		sudo visudo -c -f configure-sudoers.tmp
+		[[ $? ]] && sudo cp configure-sudoers.tmp /etc/sudoers && rm configure-sudoers.tmp
+	fi
+else
+	echo "Sudoers already configured."
+fi
+
+
+# Postfix configuration
+
+# Enable system to send administrative emails
+if [[ $(dpkg -l bsd-mailx | grep "not installed") ]]; then
+	echo $bold"Installing bsd-mailx package..."$normal
+	sudo apt-get -y install bsd-mailx
+else
+	echo "bsd-mailx package already installed."
+fi
+
+# Create alias for current user
+if [[ ! $(grep "root: $(whoami)" /etc/aliases) ]]; then
+	yesno "Create root -> $(whoami) alias?" answer y
+	if (( $? )); then
+		echo $bold"Creating alias..."$normal
+		echo "root: $(whoami)" | sudo tee -a /etc/aliases
+	fi
+else
+	echo "Mail alias for root -> $(whoami) already configured."
 fi
 
 
